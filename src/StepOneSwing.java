df5cfcb15f24c25958a2313e949aab1baec9d8ac
tuan0342 <79151156+tuan0342@users.noreply.github.com>
2022-10-22 18:17:57 +0700

    Update StepOneSwing.java

--- src/StepOneSwing.java
+++ src/StepOneSwing.java
@@ -3,8 +3,7 @@ import java.awt.*;
 
 public class StepOneSwing {
     // eheehehe
-    // sửa code nè
-       // sửa code lần nữa nè
+
 
     private JFrame frame;   // frame là khung
     JButton button;  // button là một thành phần khác
